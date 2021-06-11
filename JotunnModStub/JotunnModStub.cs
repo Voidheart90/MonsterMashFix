d2eb055dde53d8aa590dab4d639e99997d154287
Voidheart90 <59270760+Voidheart90@users.noreply.github.com>
2021-06-11 10:17:37 -0400

    Initial

--- JotunnModStub/JotunnModStub.cs
+++ JotunnModStub/JotunnModStub.cs
@@ -5,29 +5,45 @@
 // Project: JotunnModStub
 
 using BepInEx;
+using Jotunn.Managers;
 using UnityEngine;
-using BepInEx.Configuration;
-using Jotunn.Utils;
 
-namespace JotunnModStub
+namespace MonsterMashFix
 {
     [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
     [BepInDependency(Jotunn.Main.ModGuid)]
+    [BepInDependency("Monsterzz")]
     //[NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Minor)]
     internal class JotunnModStub : BaseUnityPlugin
     {
-        public const string PluginGUID = "com.jotunn.jotunnmodstub";
-        public const string PluginName = "JotunnModStub";
+        public const string PluginGUID = "com.jotunn.monstermashfix";
+        public const string PluginName = "MonsterMashFix";
         public const string PluginVersion = "0.0.1";
 
         private void Awake()
         {
-            // Do all your init stuff here
-            // Acceptable value ranges can be defined to allow configuration via a slider in the BepInEx ConfigurationManager: https://github.com/BepInEx/BepInEx.ConfigurationManager
-            Config.Bind<int>("Main Section", "Example configuration integer", 1, new ConfigDescription("This is an example config, using a range limitation for ConfigurationManager", new AcceptableValueRange<int>(0, 100)));
+            //// Do all your init stuff here
+            //// Acceptable value ranges can be defined to allow configuration via a slider in the BepInEx ConfigurationManager: https://github.com/BepInEx/BepInEx.ConfigurationManager
+            //Config.Bind<int>("Main Section", "Example configuration integer", 1, new ConfigDescription("This is an example config, using a range limitation for ConfigurationManager", new AcceptableValueRange<int>(0, 100)));
 
-            // Jotunn comes with its own Logger class to provide a consistent Log style for all mods using it
+            //// Jotunn comes with its own Logger class to provide a consistent Log style for all mods using it
             Jotunn.Logger.LogInfo("ModStub has landed");
+            PrefabManager.OnPrefabsRegistered += PrefabManager_OnPrefabsRegistered;
+        }
+
+        private void PrefabManager_OnPrefabsRegistered()
+        {
+            FixEarthTroll();
+        }
+
+        private void FixEarthTroll()
+        {
+            var prefab = PrefabManager.Instance.GetPrefab("EarthTroll");
+            if (prefab != null)
+            {
+                Jotunn.Logger.LogMessage("Patching EarthTroll");
+                prefab.AddComponent<CharacterDrop>();
+            }
         }
 
 #if DEBUG
